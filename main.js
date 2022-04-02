37ffe341e0f3e6a6f57ac14a9695ad24551ee3d0
LuisMFG <luismiguelfloresgr@gmail.com>
2022-04-02 02:54:27 -0300

    TODOS LOS DATOS DEL FETCH CAPTURADOS

--- main.js
+++ main.js
@@ -1,18 +1,57 @@
+let searchBar = document.getElementById("searchBar")
+let searchButton = document.getElementById("searchButton")
+let cardCityName = document.getElementById("cityName")
+let cardDate = document.getElementById("date")
+let cardDay = document.getElementById("day")
+let cardDescription = document.getElementById("description")
+let cardCityTemp = document.getElementById("cityTemp")
+let cityName;
 
-let apiKey = "2c0d8f240926bd0d30e9d247ad69ffdb"
-let city = "Paris"
-let days = 7
 
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
 
-fetch(url)
-    .then( res => { return res.json()})
-    .then( data => {console.log(data)})
-    .catch( err => console.log("FETCH ERROR"))
 
-//**TODO: **/
-// ** 1. Con esta API parece ser que solo puedo aceeder a solo 1 dia a la vez **/
-// ** 2. Buscar otra API o ver si lo hago solo con 1 dia 
-// ** 3. Tomar el dato del input y usarlo para buscar en la URL al presionar el boton
-// ** 4. Pq no puedo usar los iconos de FontAwesome? Quiero poner un telescopio en el boton de busqueda
-// ** 5. Pq salen esos errores en consola al usar liveServer?
+
+searchBar.addEventListener("keypress", (evt) => { 
+    if (evt.code == "Enter") {
+        let city = searchBar.value        
+        getData(city)
+    }
+})
+
+const getData = async (city) => {
+    let apiKey = "2c0d8f240926bd0d30e9d247ad69ffdb"
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    const respuesta = await fetch(url)
+    const respuestaJSON = await respuesta.json()
+    // console.log(city, respuestaJSON)
+    
+    cityName = city
+    let cityUpperCase = cityName[0].toUpperCase() + cityName.substring(1)
+    cardCityName.innerText = cityUpperCase
+    
+    let hoy = new Date()
+    let currentDay = hoy.toDateString().substring(4)
+    let currentTime = `${hoy.getHours()} : ${hoy.getMinutes()} : ${hoy.getSeconds()}`
+    cardDate.innerText = `Updated: ${currentDay} - ${currentTime} hs`     
+
+    
+    let numberDay = new Date().getDay()
+    let dayOfTheWeek = days[numberDay]
+    cardDay.innerText = dayOfTheWeek
+
+    let status = respuestaJSON.weather[0].description
+    let statusToUpperCase = status[0].toUpperCase() + status.substring(1)
+    cardDescription.innerText = statusToUpperCase
+
+    let temp = Math.round(respuestaJSON.main.temp)
+    cardCityTemp.innerText = `${temp}°C`
+}
