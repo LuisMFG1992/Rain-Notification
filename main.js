47506c3d6b8c9584e6fa1a77da59035fbf553f47
LuisMFG <luismiguelfloresgr@gmail.com>
2022-04-05 01:04:15 -0300

    PRETTIER FORMAT

--- main.js
+++ main.js
@@ -10,103 +10,97 @@ let lat;
 let long;
 
 window.addEventListener("load", () => {
-    const location = navigator.geolocation.getCurrentPosition(locationOk);
-})
+  const location = navigator.geolocation.getCurrentPosition(locationOk);
+});
 
-const locationOk = (pos) => { 
-    lat = pos.coords.latitude;
-    long = pos.coords.longitude;
-    getDataOnLoad(lat, long);
+const locationOk = (pos) => {
+  lat = pos.coords.latitude;
+  long = pos.coords.longitude;
+  getDataOnLoad(lat, long);
 };
 
 const getDataOnLoad = async (lat, long) => {
+  try {
+    if (navigator.geolocation) {
+      const url = `http://api.openweathermap.org/geo/1.0/reverse?lat=-34.58170562581985&lon=-58.450708224583714&limit=5&appid=${apiKey}`;
+      const respuesta = await fetch(url);
+      const respuestaJSON = await respuesta.json();
+      const actualCity = respuestaJSON[0].name;
 
-    try {
+      let cityName = actualCity;
+      let cityUpperCase = cityName[0].toUpperCase() + cityName.substring(1);
+      cardCityName.innerText = `City: ${cityUpperCase}`;
 
-        if (navigator.geolocation) {
-        const url = `http://api.openweathermap.org/geo/1.0/reverse?lat=-34.58170562581985&lon=-58.450708224583714&limit=5&appid=${apiKey}`;
-        const respuesta = await fetch(url);
-        const respuestaJSON = await respuesta.json();
-        const actualCity = respuestaJSON[0].name;
+      let hoy = new Date();
+      let currentDay = hoy.toDateString();
+      let currentTime = `${hoy.getHours()} : ${hoy.getMinutes()} : ${hoy.getSeconds()}`;
+      cardDate.innerText = `Updated: ${currentDay} - ${currentTime} hs`;
 
-        let cityName = actualCity;
-        let cityUpperCase = cityName[0].toUpperCase() + cityName.substring(1);
-        cardCityName.innerText = cityUpperCase;
+      // TODO: OPTIMIZAR = AL ESTAR AMBOS FETCH CON AWAIT EL PRIMERO FRENA LA PETICION DEL OTRO. NO ES PRACTICO.
+      // ** UTILIZAR LA PROPIEDAD DE COUNTRY PARA DECIR DE QUE PAIS ES LA CIUDAD QUE ESTAMOS BUSCANDO
 
-        let hoy = new Date();
-        let currentDay = hoy.toDateString();
-        let currentTime = `${hoy.getHours()} : ${hoy.getMinutes()} : ${hoy.getSeconds()}`;
-        cardDate.innerText = `Updated: ${currentDay} - ${currentTime} hs`;
+      const url2 = `https://api.openweathermap.org/data/2.5/weather?q=${actualCity}&appid=${apiKey}&units=metric`;
+      const respuesta2 = await fetch(url2);
+      const respuestaJSON2 = await respuesta2.json();
+      console.log("respuestaJSON", respuestaJSON2);
 
-        // TODO: OPTIMIZAR = AL ESTAR AMBOS FETCH CON AWAIT EL PRIMERO FRENA LA PETICION DEL OTRO. NO ES PRACTICO.
+      let status = respuestaJSON2.weather[0].description;
+      let statusToUpperCase = status[0].toUpperCase() + status.substring(1);
+      cardDescription.innerText = `Description: ${statusToUpperCase}`;
 
-        const url2 = `https://api.openweathermap.org/data/2.5/weather?q=${actualCity}&appid=${apiKey}&units=metric`;
-        const respuesta2 = await fetch(url2);
-        const respuestaJSON2 = await respuesta2.json();
-        console.log('respuestaJSON', respuestaJSON2);
-
-        let status = respuestaJSON2.weather[0].description;
-        let statusToUpperCase = status[0].toUpperCase() + status.substring(1);
-        cardDescription.innerText = statusToUpperCase;
-        
-        let temp = Math.round(respuestaJSON2.main.temp);
-        cardCityTemp.innerText = `${temp}°C`;
-
-        } else {
-            alert("ERROR EN TRY.");
-        };
-
-    } catch (err) {
-        console.log("ERROR", err);
-    };
+      let temp = Math.round(respuestaJSON2.main.temp);
+      cardCityTemp.innerText = `Temp: ${temp}°C`;
+    } else {
+      alert("ERROR EN TRY.");
+    }
+  } catch (err) {
+    console.log("ERROR", err);
+  }
 };
 
-searchBar.addEventListener("keypress", (evt) => { 
-    if (evt.code == "Enter") {
-        let city = searchBar.value        
-        getData(city)
-    }
-})
+searchBar.addEventListener("keypress", (evt) => {
+  if (evt.code == "Enter") {
+    let city = searchBar.value;
+    getData(city);
+  }
+});
 
-searchButton.addEventListener("click", () => { 
-    let city = searchBar.value
-    getData(city)
-})
-
+searchButton.addEventListener("click", () => {
+  let city = searchBar.value;
+  getData(city);
+});
 
 const getData = async (city) => {
-    try{
-        
-        // let apiKey = "2c0d8f240926bd0d30e9d247ad69ffdb"
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
-        const respuesta = await fetch(url)
-        const respuestaJSON = await respuesta.json()
+  try {
+    // let apiKey = "2c0d8f240926bd0d30e9d247ad69ffdb"
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const respuesta = await fetch(url);
+    const respuestaJSON = await respuesta.json();
 
-        if (respuesta.ok) {
+    if (respuesta.ok) {
+      console.log(respuestaJSON);
 
-            console.log(respuestaJSON)
-            
-            let cityName = respuestaJSON.name
-            let cityUpperCase = cityName[0].toUpperCase() + cityName.substring(1)
-            cardCityName.innerText = cityUpperCase
-            
-            let hoy = new Date()
-            let currentDay = hoy.toDateString()
-            let currentTime = `${hoy.getHours()} : ${hoy.getMinutes()} : ${hoy.getSeconds()}`
-            cardDate.innerText = `Updated: ${currentDay} - ${currentTime} hs`
-            
-            let status = respuestaJSON.weather[0].description
-            let statusToUpperCase = status[0].toUpperCase() + status.substring(1)
-            cardDescription.innerText = statusToUpperCase
-            
-            let temp = Math.round(respuestaJSON.main.temp)
-            cardCityTemp.innerText = `${temp}°C`
+      let cityName = respuestaJSON.name;
+      let cityUpperCase = cityName[0].toUpperCase() + cityName.substring(1);
+      cardCityName.innerText = `City: ${cityUpperCase}`;
 
-        } else {
-            alert("The name of the city or country you are searching is invalid. Please try again.")
-        }
+      let hoy = new Date();
+      let currentDay = hoy.toDateString();
+      let currentTime = `${hoy.getHours()} : ${hoy.getMinutes()} : ${hoy.getSeconds()}`;
+      cardDate.innerText = `Updated: ${currentDay} - ${currentTime} hs`;
 
-    } catch (err) {
-        console.log("ERROR", err)
+      let status = respuestaJSON.weather[0].description;
+      let statusToUpperCase = status[0].toUpperCase() + status.substring(1);
+      cardDescription.innerText = `Description: ${statusToUpperCase}`;
+
+      let temp = Math.round(respuestaJSON.main.temp);
+      cardCityTemp.innerText = `Temp: ${temp}°C`;
+    } else {
+      alert(
+        "The name of the city or country you are searching is invalid. Please try again."
+      );
     }
-}
+  } catch (err) {
+    console.log("ERROR", err);
+  }
+};
